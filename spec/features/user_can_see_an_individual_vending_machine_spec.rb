d502c27ae26cb7f316a7fe6d37b291cba858f6ee
DanHutch <danhutch5280@gmail.com>
2018-10-26 11:03:57 -0600

    almost finished

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -8,5 +8,37 @@ feature 'When a user visits a vending machine show page' do
     visit machine_path(dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+
+  end
+  scenario 'they see the name and price of each snack at that location' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    chips = dons.snacks.create(name: "chips", price: 1.50)
+    cookies = dons.snacks.create(name: "cookies", price: 3.50)
+    pretzels = dons.snacks.create(name: "pretzels", price: 2.50)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content(chips.name)
+    expect(page).to have_content(chips.price)
+
+    expect(page).to have_content(cookies.name)
+    expect(page).to have_content(cookies.price)
+
+    expect(page).to have_content(pretzels.name)
+    expect(page).to have_content(pretzels.price)
+
+  end
+
+  scenario 'they see the average price of all snacks at that location' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    chips = dons.snacks.create(name: "chips", price: 1.59)
+    cookies = dons.snacks.create(name: "cookies", price: 3.55)
+    pretzels = dons.snacks.create(name: "pretzels", price: 2.58)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content(dons.average_snack_price)
   end
 end
---
+++ spec/features/user_can_see_snack_show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'When a user visits a snack show page' do
+
+  scenario 'they see the name and price of each snack at that location' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    mikes  = owner.machines.create(location: "Mike's Bar")
+    sams  = owner.machines.create(location: "Sam's Pub")
+    chips = Snack.create(name: "chips", price: 1.50, machines: [dons, mikes, sams])
+
+    visit snack_path(chips)
+
+    expect(page).to have_content(chips.name)
+    expect(page).to have_content(chips.price)
+    expect(page).to have_content(dons.location)
+    expect(page).to have_content(mikes.location)
+    expect(page).to have_content(sams.location)
+
+
+  end
+end
+
+
+  # and I see a count of the different kinds of items in that vending machine.
